6c4efef33276e5a5558807b3294cc094baffba68
errrrdz <75832045+errrrdz@users.noreply.github.com>
2022-08-18 16:15:54 +0800

    添加element-ui

--- client/src/main.js
+++ client/src/main.js
@@ -1,25 +1,29 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import "font-awesome/css/font-awesome.min.css"
-import axios from "axios"
-import qs from 'qs';
+import Vue from "vue";
+import App from "./App.vue";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import router from "./router";
+import store from "./store";
+import "font-awesome/css/font-awesome.min.css";
+import axios from "axios";
+import qs from "qs";
 
 //导入自定义模块
 import {
-  getCurDate,
-  setSessionStorage,
-  getSessionStorage,
-  removeSessionStorage,
-  setLocalStorage,
-  getLocalStorage,
-  removeLocalStorage,
-} from './common.js'
+    getCurDate,
+    setSessionStorage,
+    getSessionStorage,
+    removeSessionStorage,
+    setLocalStorage,
+    getLocalStorage,
+    removeLocalStorage,
+} from "./common.js";
 
-Vue.config.productionTip = false
+Vue.use(ElementUI);
+
+Vue.config.productionTip = false;
 //设置axios请求url基础部分
-axios.defaults.baseURL = "http://localhost:8083"
+axios.defaults.baseURL = "http://localhost:8083";
 
 //将axios挂载到vue 使用另外的方法挂载
 Vue.prototype.$axios = axios;
@@ -33,7 +37,9 @@ Vue.prototype.$getLocalStorage = getLocalStorage;
 Vue.prototype.$removeLocalStorage = removeLocalStorage;
 
 new Vue({
-  router,
-  store,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+    router,
+    store,
+    render: function (h) {
+        return h(App);
+    },
+}).$mount("#app");
